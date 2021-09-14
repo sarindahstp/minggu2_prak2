ee7e1f56b3d750eaecb243333b267432cd8c9d8d
sarindahstp <sarindah.sitepu@gmail.com>
2021-09-14 20:45:58 +0700

    first commit

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function About(){
+        return'Nama :Sarindah Karina Br Sitepu NIM:2031710042';
+    }
+}
